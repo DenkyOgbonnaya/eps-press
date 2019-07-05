1b768c24b082a22ad719e9dd21eca6c48c5c7648
DenkyOgbonnaya <denkyogb@gmail.com>
2019-07-05 15:24:00 +0100

    add db connection

---
+++ server/models/index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+//const{MONGODB_URL} = process.env;
+
+const dbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/epspress';
+
+const connectToDb = () => {
+    mongoose.connect(dbUrl, {useNewUrlParser: true})
+    .then(() => console.log('connected to eps-press db'))
+    .catch(err => console.log(err))
+}
+
+module.exports = connectToDb;
