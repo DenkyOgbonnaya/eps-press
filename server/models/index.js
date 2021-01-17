41a6b72624349857b50e0a73752607f38c05e5f9
DenkyOgbonnaya <denkyogb@gmail.com>
2021-01-17 10:14:59 +0100

    migrated to mongo atlas and upgrade mongoose version

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const{MONGODB_URL} = process.env;
+const{MONGODB_URL, DB} = process.env;
 
 const dbUrl = process.env.MONGODB_URI || MONGODB_URL  //'mongodb://localhost:27017/epspress';
 
 const connectToDb = () => {
-    mongoose.connect(dbUrl, {useNewUrlParser: true})
+    mongoose.connect(dbUrl, {dbName: DB, useNewUrlParser: true})
     .then(() => console.log('connected to eps-press db'))
     .catch(err => console.log(err))
 }
